b105dbc854c0fbc5795a5b38bb95c10cb0b330ea
SanSugranes <santiago.sugranes@eduvaud.ch>
2020-11-23 11:05:11 +0100

    Finished recursive maze maker method

--- P_Labyrinthe/P_Labyrinthe/Program.cs
+++ P_Labyrinthe/P_Labyrinthe/Program.cs
@@ -12,15 +12,13 @@ namespace P_Labyrinthe
 
         const int GRIDSIZE = 30;
 
-        static byte[,] tabGrid = new byte[GRIDSIZE, GRIDSIZE];
+        static int[,] tabGrid = new int[GRIDSIZE, GRIDSIZE];
 
-        static int[] lastPos;
+        static int[] firstPos = new int[2];
 
         static void Main(string[] args)
         {
             rand = new Random();
-
-            tabGrid[1, 7] = 1;
 
             MakeMaze();
 
@@ -29,9 +27,12 @@ namespace P_Labyrinthe
 
         static void MakeMaze()
         {
-            int firstCell = rand.Next(GRIDSIZE);
+            int[] firstCell = GetRndBorderCell();
 
-            tabGrid[firstCell, 0] = 1;
+            tabGrid[firstCell[0], firstCell[1]] = 1;
+
+            CheckCell(firstPos[0], firstPos[1], firstCell[0], firstCell[1]);
+
 
             for (int y = 0; y < GRIDSIZE; y++)
             {
@@ -49,18 +50,159 @@ namespace P_Labyrinthe
                 }
                 Console.WriteLine();
             }
+            /*Console.SetCursorPosition(firstCell[0], firstCell[1]+1);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("o");
+            Console.ResetColor();*/
         }
 
-        static bool CheckCell(byte x, byte y)
+        static bool CheckCell(int x, int y, int lastX, int lastY)
         {
-            if (tabGrid[x + 1, y] == 1 || tabGrid[x - 1, y] == 1 || tabGrid[x, y + 1] == 1 || tabGrid[x, y - 1] == 1 || x == 0 || y == 0 || x == GRIDSIZE || y == GRIDSIZE)
+            //check des bords
+            if (x == 0 || y == 0 || x == GRIDSIZE - 1 || y == GRIDSIZE - 1)
+            {
+                return false;
+            }
+            //check des cotés
+            if ((lastX != x + 1 && tabGrid[x + 1, y] == 1) || (lastX != x - 1 && tabGrid[x - 1, y] == 1) ||
+                (lastY != y + 1 && tabGrid[x, y + 1] == 1) || (lastY != y - 1 && tabGrid[x, y - 1] == 1))
             {
                 return false;
             }
             else
             {
+                tabGrid[x, y] = 1;
+
+                /*int i = 4;
+
+                while( i > 0)
+                {
+                    switch(rand.Next(4))
+                    {
+                        case 0:
+                            {
+                                if (CheckCell(x + 1, y, x, y))
+                                { }
+                                break;
+                            }
+                        case 1:
+                            {
+                                if (CheckCell(x - 1, y, x, y))
+                                { }
+                                break;
+                            }
+                        case 2:
+                            if (CheckCell(x + 1, y, x, y))
+                            { }
+                            break;
+                    }
+                }*/
+
+                int rand2 = rand.Next(4);
+
+                switch (rand2)
+                {
+                    case 0:
+                        if (CheckCell(x + 1, y, x, y))
+                        { }
+                        else if (CheckCell(x, y + 1, x, y))
+                        { }
+                        else if (CheckCell(x - 1, y, x, y))
+                        { }
+                        else if (CheckCell(x, y - 1, x, y))
+                        { }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case 1:
+                        if (CheckCell(x, y + 1, x, y))
+                        { }
+                        else if (CheckCell(x - 1, y, x, y))
+                        { }
+                        else if (CheckCell(x, y - 1, x, y))
+                        { }
+                        else if (CheckCell(x + 1, y, x, y))
+                        { }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case 2:
+                        if (CheckCell(x - 1, y, x, y))
+                        { }
+                        else if (CheckCell(x, y - 1, x, y))
+                        { }
+                        else if (CheckCell(x + 1, y, x, y))
+                        { }
+                        else if (CheckCell(x, y + 1, x, y))
+                        { }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case 3:
+                        if (CheckCell(x, y - 1, x, y))
+                        { }
+                        else if (CheckCell(x + 1, y, x, y))
+                        { }
+                        else if (CheckCell(x, y + 1, x, y))
+                        { }
+                        else if (CheckCell(x - 1, y, x, y))
+                        { }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                }
+
                 return true;
             }
+        }
+
+        static int[] GetRndBorderCell()
+        {
+            int[] buffer;
+            switch (rand.Next(4))
+            {
+                //top
+                case 0:
+                    buffer = new int[] { rand.Next(1, GRIDSIZE), 0 };
+                    firstPos[0] = buffer[0];
+                    firstPos[1] = buffer[1];
+                    firstPos[1] += 1;
+                    return buffer;
+                    break;
+                //bottom
+                case 1:
+                    buffer = new int[] { rand.Next(1, GRIDSIZE), GRIDSIZE - 1 };
+                    firstPos[0] = buffer[0];
+                    firstPos[1] = buffer[1];
+                    firstPos[1] -= 1;
+                    return buffer;
+                    break;
+                //left
+                case 2:
+                    buffer = new int[] { 0, rand.Next(1, GRIDSIZE) };
+                    firstPos[0] = buffer[0];
+                    firstPos[1] = buffer[1];
+                    firstPos[0] += 1;
+                    return buffer;
+                    break;
+                //right
+                case 3:
+                    buffer = new int[] { GRIDSIZE - 1, rand.Next(1, GRIDSIZE) };
+                    firstPos[0] = buffer[0];
+                    firstPos[1] = buffer[1];
+                    firstPos[0] -= 1;
+                    return buffer;
+                    break;
+            }
+            return null;
         }
     }
 }
