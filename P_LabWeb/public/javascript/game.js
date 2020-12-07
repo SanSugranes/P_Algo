a7bd35a5810f1ca7455def13e0a6e5adeab6e3e9
SanSugranes <santiago.sugranes@eduvaud.ch>
2020-12-07 08:43:36 +0100

    Added initial frames / structure of gamAdded initial frames / structure of gamee

--- P_LabWeb/public/javascript/game.js
+++ P_LabWeb/public/javascript/game.js
@@ -1,9 +1,19 @@
-var tabGrid = ;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var counter = 0;
+var dt = 0;
+var now;
+var last = timestamp();
+var fps = 60;
+var step = 1/fps;
+var width = canvas.width;
+var height = canvas.height;
+var player = {width : 10, height : 10, x : 10, y : 10};
 
 document.addEventListener('keydown', function(ev) { return onkey(ev, ev.keyCode, true);  }, false);
 document.addEventListener('keyup',   function(ev) { return onkey(ev, ev.keyCode, false); }, false);
+
+frame();
 
 function onkey(ev, key, down) {
     switch(key) {
@@ -18,5 +28,32 @@ function onkey(ev, key, down) {
     }
   }
 
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(0, 0, 20, 20);
+  function frame() {
+    now = timestamp();
+    dt = dt + Math.min(1, (now - last) / 1000);
+    while(dt > step) {
+      dt = dt - step;
+      update(step);
+    }
+    render(ctx, counter, dt);
+    last = now;
+    counter++;
+    requestAnimationFrame(frame, canvas);
+  }
+
+  function update(dt) {
+  }
+
+  function render(ctx, frame, dt) {
+    ctx.clearRect(0, 0, width, height);
+    renderPlayer();
+  }
+
+  function renderPlayer(dt){
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+  }
+
+  function timestamp() {
+    return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
+  }
